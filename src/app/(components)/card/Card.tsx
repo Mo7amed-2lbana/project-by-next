d06b4462f05a058e67bacca72086bad1161ed5d6
MohamedElbana <mo7amed2lbana@gmail.com>
2025-06-01 13:39:24 +0300

    add files

--- src/app/(components)/card/Card.tsx
+++ src/app/(components)/card/Card.tsx
@@ -14,16 +14,15 @@ interface IProps {
 const Card = ({ id, image, title, discription }: IProps) => {
   return (
     <>
-      <div className="card transition-all overflow-hidden cursor-pointer">
+      <div className="card transition-all overflow-hidden">
         {/* Display the product image */}
         <img src={image} alt={title} className="h-[200px] mx-auto" />
 
         {/* Display the product title, shortened if necessary */}
-        <h2 className="my-2 font-[550]">
-          {" "}
-          <Link href={`products/${id}`}>{ellipsisChr(title, 20)}</Link>
-        </h2>
-        <p>{ellipsisChr(discription, 50)}</p>
+        <Link href={`products/${id}`} className="cursor-pointer">
+          <h2 className="my-2 font-[550]"> {ellipsisChr(title, 20)}</h2>
+          <p>{ellipsisChr(discription, 50)}</p>
+        </Link>
         {/* Button to view more details about the product */}
         <button className="bg-gray-800  text-white translate-y-[101%] text-center w-full  px-4 py-2 mt-4 rounded">
           <span>Add To Cart</span>
